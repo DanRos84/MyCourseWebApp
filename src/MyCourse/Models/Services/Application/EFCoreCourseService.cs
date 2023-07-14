4c21daf6549ef7bd17c3a93796fd7e0e335dfb79
DanRos84 <danilo.rosati.b@gmail.com>
2023-07-14 15:40:06 +0200

    Query LINQ per la selezione del dettaglio del corso - Estrazione delle entità correlate al corso

--- src/MyCourse/Models/Services/Application/EFCoreCourseService.cs
+++ src/MyCourse/Models/Services/Application/EFCoreCourseService.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using MyCourse.Models.Entities;
 using MyCourse.Models.Services.Infrastructure;
 using MyCourse.Models.ViewModels;
 
@@ -15,9 +16,35 @@ namespace MyCourse.Models.Services.Application
         {
             this.dbContext = dbContext;
         }
-        public Task<CourseDetailViewModel> GetCourseAsync(int id)
+
+        public async Task<CourseDetailViewModel> GetCourseAsync(int id)
         {
-            throw new System.NotImplementedException();
+            CourseDetailViewModel viewModel = await dbContext.Courses
+                .Where(course => course.Id == id)
+                .Select(course => new CourseDetailViewModel
+                {
+                    Id = course.Id,
+                    Title = course.Title,
+                    Description = course.Description,
+                    Author = course.Author,
+                    ImagePath = course.ImagePath,
+                    Rating = course.Rating,
+                    CurrentPrice = course.CurrentPrice,
+                    FullPrice = course.FullPrice,
+                    Lessons = course.Lessons.Select(lesson => new LessonViewModel
+                    {
+                        Id = lesson.Id,
+                        Title = lesson.Title,
+                        Description = lesson.Description,
+                        Duration = lesson.Duration
+                    }).ToList()
+                })
+                //.FirstOrDefaultAsync(); //Restituisce null se l'elenco è vuoto e non solleva mai un'eccezione
+                //.SingleOrDefaultAsync(); //Tollera il fatto che l'elenco sia vuoto e in quel caso restituisce null, oppure se l'elenco contiene più di 1 elemento, solleva un'eccezione
+                //.FirstAsync(); //Restituisce il primo elemento, ma se l'elenco è vuoto solleva un'eccezione
+                .SingleAsync(); //Restituisce il primo elemento dell'elenco, ma se l'elenco ne contiene 0 o più di 1, allora solleva un'eccezione
+
+            return viewModel;
         }
         public async Task<List<CourseViewModel>> GetCoursesAsync()
         {
