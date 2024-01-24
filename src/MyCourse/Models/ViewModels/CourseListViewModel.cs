34b631a5c1429d1cacfa2d5d377df3f32ff7ce96
DanRos84 <danilo.rosati.b@gmail.com>
2024-01-24 20:08:00 +0100

    The-pagination-ViewComponent-is-now-reusable

--- src/MyCourse/Models/ViewModels/CourseListViewModel.cs
+++ src/MyCourse/Models/ViewModels/CourseListViewModel.cs
@@ -4,9 +4,20 @@ using MyCourse.Models.InputModels;
 namespace MyCourse.Models.ViewModels
 {
     
-    public class CourseListViewModel
+    public class CourseListViewModel : IPaginationInfo
     {
         public ListViewModel<CourseViewModel> Courses { get; set; }
         public CourseListInputModel Input { get; set; }
+
+
+
+        #region Implementation IPaginationInfo
+        int IPaginationInfo.CurrentPage => Input.Page;
+        int IPaginationInfo.TotalResults => Courses.TotalCount;
+        int IPaginationInfo.ResultsPerPage => Input.Limit;
+        string IPaginationInfo.Search => Input.Search;
+        string IPaginationInfo.OrderBy => Input.OrderBy;
+        bool IPaginationInfo.Ascending => Input.Ascending;
+        #endregion
     }
 }
