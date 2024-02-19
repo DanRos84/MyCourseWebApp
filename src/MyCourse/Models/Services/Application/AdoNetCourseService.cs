342855e3a79bda8e3b7d14acbbfac1111ae4f9e3
DanRos84 <danilo.rosati.b@gmail.com>
2024-02-19 12:44:45 +0100

    CourseTitleUnavailableException-created

--- src/MyCourse/Models/Services/Application/AdoNetCourseService.cs
+++ src/MyCourse/Models/Services/Application/AdoNetCourseService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MyCourse.Models.Exceptions;
@@ -119,12 +120,19 @@ namespace MyCourse.Models.Services.Application
             string title = inputModel.Title;
             string author = "Mario Rossi";
 
-            var dataSet = await db.QueryAsync($@"INSERT INTO Courses (Title, Author, ImagePath, CurrentPrice_Currency, CurrentPrice_Amount, FullPrice_Currency, FullPrice_Amount) VALUES ({title}, {author}, '/Courses/default.png', 'EUR', 0, 'EUR', 0);
+            try
+            {
+                DataSet dataSet = await db.QueryAsync($@"INSERT INTO Courses (Title, Author, ImagePath, CurrentPrice_Currency, CurrentPrice_Amount, FullPrice_Currency, FullPrice_Amount) VALUES ({title}, {author}, '/Courses/default.png', 'EUR', 0, 'EUR', 0);
                                                  SELECT last_insert_rowid();");
 
-            int courseId = Convert.ToInt32(dataSet.Tables[0].Rows[0][0]);
-            CourseDetailViewModel course = await GetCourseAsync(courseId);
-            return course;
+                int courseId = Convert.ToInt32(dataSet.Tables[0].Rows[0][0]);
+                CourseDetailViewModel course = await GetCourseAsync(courseId);
+                return course;
+            }
+            catch (SqliteException exc) when (exc.SqliteErrorCode == 19)
+            {
+                throw new CourseTitleUnavailableException(title, exc);
+            }
         }
     }
 }
