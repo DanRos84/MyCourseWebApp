7c10307b8afb2015beca47adea4a767fad3a3ff2
DanRos84 <danilo.rosati.b@gmail.com>
2024-02-27 12:04:16 +0100

    Added-HTML-sanitization-of-the-course-description-with-Ganss.HtmlSanitizer

---
+++ src/MyCourse/Customizations/TagHelpers/HtmlSanitizeTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Ganss.Xss;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace MyCourse.Customizations.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "html-sanitize")]
+    public class HtmlSanitizeTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            //ottengo il contenuto del Tag
+            TagHelperContent tagHelperContent = await output.GetChildContentAsync(NullHtmlEncoder.Default);
+            string content = tagHelperContent.GetContent(NullHtmlEncoder.Default);
+
+            var sanitizer = new HtmlSanitizer();
+            content = sanitizer.Sanitize(content);
+
+            //remimposto il contenuto del Tag
+            output.Content.SetHtmlContent(content);
+
+        }
+    }
+}
