db171afaf9998e8e49c716ac2dccc4493119a2f6
DanRos84 <danilo.rosati.b@gmail.com>
2024-03-04 19:16:17 +0100

    Used-Magick.NET-to-resize-the-user-uploaded-image

---
+++ src/MyCourse/Models/Services/Infrastructure/MagickNetImagePersister.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using ImageMagick;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace MyCourse.Models.Services.Infrastructure
+{
+    public class MagickNetImagePersister : IImagePersister
+    {
+        private readonly IWebHostEnvironment env;
+
+        public MagickNetImagePersister(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        public async Task<string> SaveCourseImageAsync(int courseId, IFormFile formFile)
+        {
+            //Salvare il file
+            string path = $"/Course/{courseId}.jpg";
+            string physicalPath = Path.Combine(env.WebRootPath, "Courses", $"{courseId}.jpg");
+
+            using Stream inputStream = formFile.OpenReadStream();
+            using IMagickImage image = new MagickImage(inputStream);
+
+            //Manipolare l'immagine
+            int width = 300;  //Esercizio: ottenere questi valori dalla configurazione
+            int height = 300;
+            MagickGeometry resizeGeometry = new MagickGeometry(width, height)
+            {
+                FillArea = true
+            };
+            image.Resize(resizeGeometry);
+            image.Crop(width, width, Gravity.Northwest);
+
+            image.Quality = 70;
+            image.Write(physicalPath, MagickFormat.Jpg);
+
+            // Restituire il percorso del File
+            return path;
+        }
+    }
+}
